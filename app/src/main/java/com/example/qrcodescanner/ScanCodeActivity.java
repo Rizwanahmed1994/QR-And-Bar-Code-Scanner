f2b251ff1f55cf6edb214c3bdccf9676357c9f4b
Rizwanahmed1994 <rizwanzafer2@gmail.com>
2020-06-29 00:01:18 +0500

    QR and Bar Code Scanner

---
+++ app/src/main/java/com/example/qrcodescanner/ScanCodeActivity.java
@@ -0,0 +1,42 @@
+package com.example.qrcodescanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    ZXingScannerView scannerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        MainActivity.resultTextView.setText(result.getText());
+        onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+}
